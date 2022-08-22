b38dde24bea38218e39b84627868b3670e63a38d
xv88ddd <xvddd8670@gmail.com>
2022-08-22 16:24:37 +0200

    'commit'

--- cell.py
+++ cell.py
@@ -1,10 +1,25 @@
+import pygame
 import random
 
 class Cell:
-    def __init__(self):
+    ##
+    def __init__(self, position_x, position_y):
         self.type = 0
         self.rotate = 0
         self.energy = 0
         self.type_next = 0
-    def check_cell(self):
+        self.position_x = position_x
+        self.position_y = position_y
+        ##
+        self.color_list = {'green' : (0, 255, 0),
+                           'red' : (255, 0, 0),
+                           'blue' : (0, 0, 255)}
+        self.active_color = self.color_list['green']
+    ##
+    def check_cell(self, cells_list):
         self.rotate = random.randrange(0, 7)
+        print(cells_list[3][6].position_x)
+        print(cells_list[3][6].position_y)
+    ##
+    def render(self, screen):
+        pygame.draw.rect(screen, self.active_color, pygame.Rect(self.position_x*6, self.position_y*6, 5, 5), 3)
--- cell_automat_main.py
+++ cell_automat_main.py
@@ -2,23 +2,39 @@ import pygame
 import pygame_gui
 ##################
 import gui
-
+import cell
+#init
 pygame.init()
 
 infoObject = pygame.display.Info()
 window_width = infoObject.current_w#1920
 window_height = infoObject.current_h#1080
 screen = pygame.display.set_mode((window_width, window_height), pygame.FULLSCREEN)
-
 pygame.display.set_caption('cell_automat')
-
 manager = pygame_gui.UIManager((window_width, window_height))
 
 #gui#
 button = gui.Button(0, 0, 150, 50, 'text', manager)
 
+#objects
+#create cells list
+size_x = 100
+size_y = 100
+cells_list = []
+i_in_while = 0
+while i_in_while < size_x:
+    j_in_while = 0
+    cells_y = []
+    while j_in_while < size_y:
+        cells_y.append(cell.Cell(i_in_while, j_in_while))
+        j_in_while += 1
+    cells_list.append(cells_y)
+    del(cells_y)
+    i_in_while += 1
+
+cells_list[0][3].check_cell(cells_list)
+#main while 
 clock = pygame.time.Clock()
-##
 running = True
 while running:
     time_delta = clock.tick(60)/1000.0
@@ -38,7 +54,11 @@ while running:
     screen.fill((0, 0, 0))
     manager.draw_ui(screen)
 
-    pygame.draw.rect(screen, (0, 255, 0), pygame.Rect(2, 50, 50, 40), 3)
+    #pygame.draw.rect(screen, (0, 255, 0), pygame.Rect(2, 50, 50, 40), 3)
+    for cells_block in cells_list:
+        for cell_ in cells_block:
+            cell_.render(screen)
+
     pygame.display.update()
 
 
